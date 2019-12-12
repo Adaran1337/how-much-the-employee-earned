612c5550a67f9aa7df33e9b47d6a52b9014ca14a
Александр Матвеев <55204274+Adaran1337@users.noreply.github.com>
2019-12-13 00:00:14 +0300

    Update code.ino
    Added button to select a mode:
    1 How much money the employee received
    2 How much the employee did not receive money.

--- code.ino
+++ code.ino
@@ -1,8 +1,13 @@
 #include <LiquidCrystal.h>
 #define US 6
+#define BUTTON 7
 LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
 int cm = 0;
-int salary=lostSalary=0;
+int salary=0;
+int lostSalary=0;
+int rate=5;
+bool switchMode=false;
+bool butt_state=false;
 long readUltrasonicDistance()
 {
 pinMode(US, OUTPUT);
@@ -17,14 +22,38 @@ return pulseIn(US, HIGH);
 
 void setup() {
   Serial.begin(9600);
+  pinMode(BUTTON,INPUT_PULLUP);
   lcd.begin(16, 2);
 }
+bool buttonState(){
 
+}
 void loop() {
   lcd.setCursor(0, 0);
 cm = 0.01723 * readUltrasonicDistance();
   lcd.clear();
   lcd.print(cm);
   lcd.print(" cm to odject");
+  lcd.setCursor(0, 1);
+  if(cm<100)
+  salary+=rate;
+  else
+  lostSalary+=rate;
+  if(!digitalRead(BUTTON)&&butt_state==false){
+    if(switchMode)
+      switchMode=false;
+    else 
+      switchMode=true;
+    butt_state=true;
+  }
+if(digitalRead(BUTTON)&&butt_state==true)
+  butt_state=false;
+  if(switchMode){
+  lcd.print("Salary: ");
+    lcd.print(salary);}
+  else{
+    lcd.print("LostSalary: ");
+    lcd.print(lostSalary);
+  }
 delay(100); 
 }
